a28304989c6cfca4d402b68262eca1386cc1cc40
Miguel G. Sanguino <sanguino@me.com>
2021-03-15 21:00:10 +0100

    add config rdsinstance

--- src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/Application.java
+++ src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/Application.java
@@ -3,9 +3,11 @@ package es.codeurjc.mca.practica_1_cloud_ordinaria_2021;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableRdsInstance(dbInstanceIdentifier="${cloud.aws.rds.dbInstanceIdentifier}", password="${cloud.aws.rds.rdsp2.password}")
 public class Application {
 
 	public static void main(String[] args) {
