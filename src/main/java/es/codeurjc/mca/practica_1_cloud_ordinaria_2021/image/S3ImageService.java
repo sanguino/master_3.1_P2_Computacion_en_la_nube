22626c32b7198b06a145f0f98829b2eebae91c1d
Miguel G. Sanguino <sanguino@me.com>
2021-03-15 21:00:58 +0100

    refactor s3imageservice to spring cloud

--- src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3ImageService.java
+++ src/main/java/es/codeurjc/mca/practica_1_cloud_ordinaria_2021/image/S3ImageService.java
@@ -1,16 +1,10 @@
 package es.codeurjc.mca.practica_1_cloud_ordinaria_2021.image;
 
-import java.io.File;
-import java.util.UUID;
-
-import javax.annotation.PostConstruct;
-
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
@@ -18,56 +12,43 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.UUID;
+
 @Service("storageService")
 @Profile("production")
 public class S3ImageService implements ImageService {
 
-    @Value("${amazon.s3.bucket-name}")
-    private String bucketName;
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
 
-    @Value("${amazon.s3.endpoint}")
-    private String url;
+  @Value("${amazon.s3.endpoint}")
+  private String url;
 
-    @Value("${amazon.s3.region}")
-    private String region;
+  @Autowired
+  private AmazonS3 s3;
 
-    private AmazonS3 s3;
+  @Override
+  public String createImage(MultipartFile file) {
+    String fileName = "image_" + UUID.randomUUID() + "_" + file.getOriginalFilename();
+    try {
 
-    @PostConstruct
-    public void initS3Client() {
-        this.s3 = AmazonS3ClientBuilder.standard()
-                    .withRegion(Regions.fromName(region))
-                    .build();
-        createBucketIfNotExist();
+      ObjectMetadata objectMetadata = new ObjectMetadata();
+      objectMetadata.setContentType(file.getContentType());
+
+      TransferManager transferManager = TransferManagerBuilder.defaultTransferManager();
+      transferManager.upload(bucket, fileName, file.getInputStream(), objectMetadata);
+    } catch (Exception ex) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't save image on S3", ex);
     }
+    return url + fileName;
+  }
 
-    private void createBucketIfNotExist() {
-        if (!s3.doesBucketExistV2(bucketName)) {
-            s3.createBucket(bucketName);
-        }
-    }
-
-    @Override
-    public String createImage(MultipartFile multiPartFile) {
-        String fileName = "image_" + UUID.randomUUID() + "_" +multiPartFile.getOriginalFilename();
-        File file = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
-        try {
-            multiPartFile.transferTo(file);
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Can't save image on S3", ex);
-        }
-        PutObjectRequest por = new PutObjectRequest(bucketName, fileName, file);
-        por.setCannedAcl(CannedAccessControlList.PublicRead);
-        s3.putObject(por);
-        file.delete();
-        return url + fileName;
-    }
-
-    @Override
-    public void deleteImage(String image) {
-        String[] tokens = image.split("/");
-        String fileName = tokens[tokens.length - 1];
-        s3.deleteObject(bucketName, fileName);
-    }
+  @Override
+  public void deleteImage(String image) {
+    String[] tokens = image.split("/");
+    String fileName = tokens[tokens.length - 1];
+    s3.deleteObject(bucket, fileName);
+  }
 
 }
+
